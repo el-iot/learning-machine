5f51ad9daf3514fe186dd36cb88e22a237a57b0b
elra <erampono@gmail.com>
2020-02-03 14:40:59 -0800

    Reformat and modified printing functionality
    Renamed class-methods (now 'forwards' and 'backwards') and added
    printing

--- networks/numpy/no_hidden_layer.py
+++ networks/numpy/no_hidden_layer.py
@@ -12,12 +12,9 @@ DEBUG = False
 EPOCHS = 15000
 BIAS = True
 
-activation = getattr(
-    __import__("activation_functions", fromlist=[ACTIVATION]), ACTIVATION
-)
+activation = getattr(__import__("activation_functions", fromlist=[ACTIVATION]), ACTIVATION)
 activation_prime = getattr(
-    __import__("activation_functions", fromlist=[ACTIVATION + "_prime"]),
-    ACTIVATION + "_prime",
+    __import__("activation_functions", fromlist=[ACTIVATION + "_prime"]), ACTIVATION + "_prime"
 )
 
 numpy.random.seed(0)
@@ -31,20 +28,14 @@ class NeuralNetwork:
         self.loss = []
         self.verbose = DEBUG
 
-    def feedforward(self, X, y):
-        self._print(f"y: {y}")
+    def forwards(self, X, y):
         X = numpy.append(X, 1) if BIAS else X
-        self._print(f"X: {X}")
         self.output_layer_input = numpy.dot(X, self.w0)
-        self._print(f"output_layer_input: {self.output_layer_input}")
         self.output = activation(self.output_layer_input)
-        self._print(f"output: {self.output}")
 
-    def backprop(self, y):
+    def backwards(self, y):
         output_error = y - self.output
-        self._print(f"output_error: {output_error}")
         output_delta = output_error * activation_prime(self.output_layer_input)
-        self._print(f"output_delta: {output_delta}")
 
         self.w0 += self.learning_rate * output_delta
         self.loss += [abs(y - self.output) ** 2]
@@ -52,17 +43,23 @@ class NeuralNetwork:
         self._print("-" * 88)
 
     def process(self, X, y):
-        self.feedforward(X, y)
-        self.backprop(y)
+        """
+        Perform one feed-forward and backpropagation cycle
+        """
+        self.forwards(X, y)
+        self.backwards(y)
 
     def train(self, epochs):
+        """
+        Train the model on basic_data.csv
+        """
         data = pandas.read_csv("../data/basic_data.csv", index_col="Unnamed: 0").head(100)
         for epoch in range(epochs):
             for _, row in data.iterrows():
                 X = numpy.array([row[-1]])
                 y = numpy.array([row[-1]])
                 self.process(X, y)
-            print(f"Loss: {sum(self.loss)/data.shape[0]}")
+            self._print(f"Loss: {sum(self.loss)/data.shape[0]}")
             self.loss = []
             self.learning_rate *= self.learning_rate_depreciation
 
