146b22ae355f1184786b7c40219d8f04f27f1cde
el <erampono@gmail.com>
2020-02-08 23:07:38 -0800

    added bias

--- models/neural_network/model/neural_network.py
+++ models/neural_network/model/neural_network.py
@@ -31,27 +31,32 @@ class NeuralNetwork:
         learning_rate: float = 10e-6,
         loss_function=lambda x: x ** 2,
         name="learning-machine",
+        has_bias=False,
     ):
         """
         Instantiate the model
         """
         self.weights = {
-            idx: numpy.random.randn(shape[idx], shape[idx + 1]) for idx in range(len(shape) - 1)
+            idx: numpy.random.randn(shape[idx] + (has_bias if idx == 0 else 0), shape[idx + 1])
+            for idx in range(len(shape) - 1)
         }
+
         self.deltas = {}
+        self.has_bias = has_bias
         self.learning_rate = learning_rate
         self.logger = structlog.get_logger(name)
         self.loss = 0
         self.loss_function = loss_function
         self.n_layers = len(shape) - 1
-        self.values = {
-            level: {"input": None, "output": None} for level in range(len(shape) - 1)
-        }
+        self.values = {level: {"input": None, "output": None} for level in range(len(shape) - 1)}
 
     def forwards(self, X):
         """
         Feed the data forward
         """
+        if self.has_bias:
+            X = numpy.append(X, 1)
+
         _in = X
 
         for layer_idx in range(self.n_layers):
@@ -70,13 +75,18 @@ class NeuralNetwork:
 
             if layer_idx == self.n_layers - 1:
                 error = _out - self.values[layer_idx]["output"]
-                self.loss += self.loss_function(error[0][0])
+                self.loss += self.loss_function(self.scalar(error))
             else:
                 error = numpy.dot(self.deltas[layer_idx + 1], self.weights[layer_idx + 1].T)
 
             delta = error * activation_prime(self.values[layer_idx]["input"])
             self.weights[layer_idx] += self.learning_rate * delta
             self.deltas[layer_idx] = delta
+
+    def scalar(self, value):
+        if isinstance(value, (int, float)):
+            return value
+        return self.scalar(value[0])
 
     def process(self, X, y):
         """
@@ -101,5 +111,5 @@ class NeuralNetwork:
 
 if __name__ == "__main__":
 
-    nn = NeuralNetwork([3, 2, 1], loss_function=abs, learning_rate=0.000005)
+    nn = NeuralNetwork([3, 2, 1], has_bias=True, loss_function=abs, learning_rate=0.000005)
     nn.train(epochs=EPOCHS)
